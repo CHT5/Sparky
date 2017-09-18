7cffa1737230eb9f155811263b6e68c61c1bcebc
Trinitrotoluene <pmichallet00@gmail.com>
2017-09-18 21:10:29 +0100

    DI + EfCore
    Setup EfCore and added CwsContext to the services collection. Models for Tags and Users created and should be ready to go.

---
+++ CWSBot/CWSBot/Interaction/CwsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CWSBot.Interaction
+{
+    public class CwsContext : DbContext
+    {
+        public CwsContext()
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlite("Data Source=cws-bot.db");
+        }
+    }
+}
