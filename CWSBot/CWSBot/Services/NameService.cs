a0d87a78f32260c8d1927dc5c30e85104ced9b3f
Izumemori <rannick10@tutanota.com>
2017-10-27 12:54:24 +0200

    Fixing naming on my end

--- CWSBot/CWSBot/Services/NameService.cs
+++ CWSBot/CWSBot/Services/NameService.cs
@@ -10,48 +10,44 @@ namespace CWSBot.Services
         private const string InvalidNickname = "💩";
 
         private static string[] _attentionSeekingPrefixes = new [] 
-                                                            {
-                                                                "!"
-                                                            };
+                                {
+                                    "!"
+                                };
 
         public NameService(DiscordSocketClient client)
         {
-            client.UserJoined += (user) => {_ = Task.Run(() => CheckUser(user)); return Task.CompletedTask;};
+            client.UserJoined += (user) => {_ = Task.Run(() => CheckUserAsync(user)); return Task.CompletedTask;};
             // I'll figure this out on a later date
             //client.GuildMemberUpdated += (before, user) => {_ = Task.Run(() => CheckUser(user, before)); return Task.CompletedTask;};
         }
 
         // This will change a users Nickname on Join if
-        /// They seek attention (nickname starts with a !)
+        /// They seek attention (Name starts with a !)
         /// They have >33% non ascii chars in their name
         // and send a message telling the user about it in mod_logs
-        private async Task CheckUser(SocketGuildUser user)
+        private async Task CheckUserAsync(SocketGuildUser user)
         {
             if (user.IsBot) return;
 
             if (user.Nickname != null) return;
 
             if (_attentionSeekingPrefixes.Any(x => user.Username.StartsWith(x)))
-                await ChangeNickname(user, true);
+                await ChangeNicknameAsync(user, true);
 
             bool IsCharMentionable(char c)
-            {
-                int numericChar = (int)c;
-
-                return c >= 34 && c <= 126;
-            }
+                => c >= 34 && c <= 126;
 
             if (user.Username.Sum(x => IsCharMentionable(x) ? 1 : 0) < (user.Username.Length * 0.33))
-                await ChangeNickname(user);
+                await ChangeNicknameAsync(user);
         }
 
-        private async Task ChangeNickname(SocketGuildUser user, bool attentionSeeking = false)
+        private async Task ChangeNicknameAsync(SocketGuildUser user, bool attentionSeeking = false)
         {
             await user.ModifyAsync(x => x.Nickname = InvalidNickname);
             var modChannel = user.Guild.TextChannels.FirstOrDefault(x => x.Name.ToLower() == "mod_logs");
             if (modChannel is null)
             {
-                Console.WriteLine($"No mod-log channel found!");
+                Console.WriteLine($"No mod_log channel found!");
                 return;
             }
 
