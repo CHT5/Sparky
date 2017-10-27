0b010d1bbf838fa94f650ad047820575864d576f
Izumemori <rannick10@tutanota.com>
2017-10-27 20:31:35 +0200

    Further cleanup on TagsModule

--- CWSBot/CWSBot/Modules/Public/TagsModule.cs
+++ CWSBot/CWSBot/Modules/Public/TagsModule.cs
@@ -13,7 +13,11 @@ namespace CWSBot.Modules.Public
     [Group("tag"), Alias("tags")]
     public class TagsModule : ModuleBase
     {
-        private CwsContext _dbContext;
+        private const string DeniedEmoji = "\u274c";
+
+        private const string AcceptedEmoji = "\u2611";
+
+        private readonly CwsContext _dbContext;
 
         private Color _embedColor;
 
@@ -28,13 +32,37 @@ namespace CWSBot.Modules.Public
             else _embedColor = Color.Purple;
         }
 
+        protected override void AfterExecute(CommandInfo command)
+        {
+            _dbContext?.SaveChanges();
+        }
+
         public TagsModule(CwsContext dbContext)
         {
             _dbContext = dbContext;
         }
 
         [Command]
-        public async Task TagAsync(string name)
+        [Alias("list")]
+        public async Task ListTagsAsync()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            var storedTags = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id);
+            string descString = string.Join(", ", storedTags.Select(x => x.Name));
+
+            if (descString.Length > 2000)
+                descString = descString.Substring(0, 2000);
+
+            builder.WithTitle($"Server Tags for {Context.Guild.Name}")
+                   .WithColor(_embedColor)
+                   .WithDescription(descString);
+
+            await ReplyAsync("", false, builder.Build());
+        }
+
+        [Command]
+        public async Task TagAsync([Remainder] string name)
         {
             //initialise the builder so we can use it later
             var builder = new EmbedBuilder();
@@ -78,14 +106,12 @@ namespace CWSBot.Modules.Public
                     }
 
                     recievedTag.Uses += 1;
-                    _dbContext.SaveChanges();
-
                     return;
                 }
-                //build our embed and send it off!
+
                 builder.WithTitle(recievedTag.Name)
-                    .WithDescription(recievedTag.Content)
-                    .WithFooter("Owner: " + recievedTag.CreatorName);
+                       .WithDescription(recievedTag.Content)
+                       .WithFooter("Owner: " + recievedTag.CreatorName);
 
                 await ReplyAsync("", embed: builder.Build());
                 recievedTag.Uses += 1;
@@ -103,165 +129,154 @@ namespace CWSBot.Modules.Public
             if (tagNames.Count() == 0)
             {
                 builder.WithTitle("No matching tags found")
-                    .WithDescription("There were no tags found matching that description. Use <tag list> to find some!");
+                       .WithDescription("There were no tags found matching that description. Use <tag list> to find some!");
+
                 await ReplyAsync("", false, builder.Build());
 
                 return;
             }
 
-            string response = string.Join("", tagNames.Select((x, i) => $"{x.Name}{(i % 3 == 0 && i != 0 ? "\n" : ", ")}"));
+            string response = string.Join("", tagNames.Select((x, i) => $"{x.Name}{(i % 4 == 0 && i != 0 ? "\n" : ", ")}"));
             response = response.Remove(response.Length - 2);
 
             builder.WithTitle("Tag not found!")
-                .WithDescription("Similar tags: \n" + response);
-            //send result to user
+                   .WithDescription("Similar tags: \n" + response);
+
             await ReplyAsync("", embed: builder.Build());
         }
 
-        [Command("mod"), RequireUserPermission(GuildPermission.ManageGuild)]
-        public async Task ModTagAsync(string args, string Name, [Remainder] string content = null)
+        [Group("mod")]
+        [RequireUserPermission(GuildPermission.ManageGuild)]
+        public class ModTagModule : ModuleBase<SocketCommandContext>
         {
-            switch (args)
+            private readonly CwsContext _dbContext;
+
+            public ModTagModule(CwsContext dbContext)
+                => this._dbContext = dbContext;
+
+            [Command("delete")]
+            [Alias("rm", "remove")]
+            public async Task DeleteTagAsync(string name)
             {
-                case "delete":
-                    Tag tag = _dbContext.Tags.SingleOrDefault(x => x.Name.ToLower() == Name.ToLower() && x.GuildId == Context.Guild.Id);
+                var tag = _dbContext.Tags.SingleOrDefault(x => x.Name.ToLower() == name.ToLower() && x.GuildId == Context.Guild.Id);
 
-                    if (tag is null)
-                        await Context.Message.AddReactionAsync(new Emoji("\u274c")); // Denied emoji
-                    else
-                    {
-                        _dbContext.Remove(tag);
-                        _dbContext.SaveChanges();
+                if (tag is null)
+                {
+                    await Context.Message.AddReactionAsync(new Emoji(DeniedEmoji));
+                    return;
+                }
 
-                        await Context.Message.AddReactionAsync(new Emoji("\u2611")); // Accepted emoji
-                    }
-                    break;
-                case "edit":
-                    Tag tag_ = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id).SingleOrDefault(x => x.Name.ToLower() == Name.ToLower());
+                _dbContext.Remove(tag);
 
-                    if (tag_ is null)
-                        await Context.Message.AddReactionAsync(new Emoji("\u274c")); // Denied emoji
-                    else
-                    {
-                        tag_.Content = content;
-                        _dbContext.SaveChanges();
+                await Context.Message.AddReactionAsync(new Emoji(AcceptedEmoji));
+            }
 
-                        await Context.Message.AddReactionAsync(new Emoji("\u2611")); // Accepted emoji
-                    }
-                    break;
-                default:
-                    await ReplyAsync("usage: <tag mod delete/edit>");
-                    break;
+            [Command("edit")]
+            public async Task EditTagAsync(string name, [Remainder] string content)
+            {
+                var tag = _dbContext.Tags.SingleOrDefault(x => x.Name.ToLower() == name.ToLower() && x.GuildId == Context.Guild.Id);
+
+                if (tag is null)
+                {
+                    await Context.Message.AddReactionAsync(new Emoji(DeniedEmoji));
+                    return;
+                }
+
+                tag.Content = content;
+
+                await Context.Message.AddReactionAsync(new Emoji(AcceptedEmoji));
             }
         }
-        [Command("create")]
-        public async Task CreateTagAsync(string Name, [Remainder] string Content)
-        {
-            if (_dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id).Any(x => x.Name.ToLower() == Name.ToLower()) || (Context.User as SocketGuildUser).Roles.Any(x => x.Name.ToLower() == "learning"))
-                await Context.Message.AddReactionAsync(new Emoji("\u274c")); // Denied emoji
-            else
-            {
-                _dbContext.Add(new Tag
-                {
-                    Name = Name,
-                    Content = Content,
-                    CreatorName = Context.User.Username,
-                    CreatorId = Context.User.Id,
-                    CreatedAt = DateTimeOffset.Now,
-                    Uses = 0,
-                    GuildId = Context.Guild.Id
-                });
 
-                _dbContext.SaveChanges();
-                await Context.Message.AddReactionAsync(new Emoji("\u2611")); // Accepted emoji
+        [Command("create")]
+        public async Task CreateTagAsync(string name, [Remainder] string content)
+        {
+            if (_dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id).Any(x => x.Name.ToLower() == name.ToLower()) || (Context.User as SocketGuildUser).Roles.Any(x => x.Name.ToLower() == "learning"))
+            {
+                await Context.Message.AddReactionAsync(new Emoji(DeniedEmoji));
+                return;
             }
+
+            _dbContext.Add(new Tag
+            {
+                Name = name,
+                Content = content,
+                CreatorName = Context.User.Username,
+                CreatorId = Context.User.Id,
+                CreatedAt = DateTimeOffset.Now,
+                Uses = 0,
+                GuildId = Context.Guild.Id
+            });
+
+            await Context.Message.AddReactionAsync(new Emoji(AcceptedEmoji));
         }
 
         [Command("delete")]
-        public async Task DeleteTagAsync(string Name)
+        public async Task DeleteTagAsync(string name)
         {
-            Tag tag = _dbContext.Tags.SingleOrDefault(x => x.Name.ToLower() == Name.ToLower() && x.GuildId == Context.Guild.Id);
+            Tag tag = _dbContext.Tags.SingleOrDefault(x => x.Name.ToLower() == name.ToLower() && x.GuildId == Context.Guild.Id);
 
             if (tag is null || tag.CreatorId != Context.User.Id)
-                await Context.Message.AddReactionAsync(new Emoji("\u274c")); // Denied emoji
-            else
             {
-                _dbContext.Remove(tag);
-                _dbContext.SaveChanges();
-
-                await Context.Message.AddReactionAsync(new Emoji("\u2611")); // Accepted emoji
+                await Context.Message.AddReactionAsync(new Emoji(DeniedEmoji));
+                return;
             }
+            _dbContext.Remove(tag);
+
+            await Context.Message.AddReactionAsync(new Emoji(AcceptedEmoji));
         }
 
         [Command("edit")]
-        public async Task EditTagAsync(string Name, [Remainder] string Content)
+        public async Task EditTagAsync(string name, [Remainder] string content)
         {
-            Tag tag = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id).SingleOrDefault(x => x.Name.ToLower() == Name.ToLower());
+            var tag = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id).SingleOrDefault(x => x.Name.ToLower() == name.ToLower());
 
             if (tag is null || tag.CreatorId != Context.User.Id)
-                await Context.Message.AddReactionAsync(new Emoji("\u274c")); // Denied emoji
-            else
             {
-                tag.Content = Content;
-                _dbContext.SaveChanges();
-
-                await Context.Message.AddReactionAsync(new Emoji("\u2611")); // Accepted emoji
+                await Context.Message.AddReactionAsync(new Emoji(DeniedEmoji));
+                return;
             }
+
+            tag.Content = content;
+
+            await Context.Message.AddReactionAsync(new Emoji(AcceptedEmoji));
         }
 
         [Command("chown")]
-        public async Task ChangeOwnerAsync(string Name, SocketGuildUser NewOwner)
+        public async Task ChangeOwnerAsync(string name, SocketGuildUser newOwner)
         {
-            Tag tag = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id).SingleOrDefault(x => x.Name.ToLower() == Name.ToLower());
+            var tag = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id).SingleOrDefault(x => x.Name.ToLower() == name.ToLower());
 
             if (tag is null || tag.CreatorId != Context.User.Id)
-                await Context.Message.AddReactionAsync(new Emoji("\u274c")); // Denied emoji
-            else
             {
-                tag.CreatorId = NewOwner.Id;
-                tag.CreatorName = NewOwner.Username;
-
-                _dbContext.SaveChanges();
-
-                await Context.Message.AddReactionAsync(new Emoji("\u2611")); // Accepted emoji
+                await Context.Message.AddReactionAsync(new Emoji(DeniedEmoji));
+                return;
             }
+
+            tag.CreatorId = newOwner.Id;
+            tag.CreatorName = newOwner.Username;
+
+            await Context.Message.AddReactionAsync(new Emoji(AcceptedEmoji));
         }
 
         [Command("info")]
-        public async Task TagInfoAsync(string Name)
+        public async Task TagInfoAsync(string name)
         {
-            Tag tag = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id).SingleOrDefault(x => x.Name.ToLower() == Name.ToLower());
+            var tag = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id).SingleOrDefault(x => x.Name.ToLower() == name.ToLower());
 
             if (tag is null)
-                await Context.Message.AddReactionAsync(new Emoji("\u274c")); // Denied emoji
-            else
             {
-                var builder = new EmbedBuilder()
-                                .WithColor(98, 31, 193)
-                                .AddField("Owner:", tag.CreatorName)
-                                .AddField("Created at:", tag.CreatedAt.ToString("ddd MMM dd HH: mm:ss"))
-                                .AddField("Content:", tag.Content)
-                                .AddField("Uses", tag.Uses);
-
-                await ReplyAsync("", false, builder.Build());
+                await Context.Message.AddReactionAsync(new Emoji(DeniedEmoji));
+                return;
             }
-        }
+            var builder = new EmbedBuilder()
+                            .WithColor(_embedColor)
+                            .AddField("Owner:", tag.CreatorName)
+                            .AddField("Created at:", tag.CreatedAt.ToString("ddd MMM dd HH: mm:ss"))
+                            .AddField("Content:", tag.Content)
+                            .AddField("Uses", tag.Uses);
 
-        [Command("list")]
-        public async Task ListTagsAsync(string Name = "")
-        {
-            EmbedBuilder builder = new EmbedBuilder();
-
-            var storedTags = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id);
-            string descString = string.Join(", ", storedTags.Select(x => x.Name));
-
-            if (descString.Length > 2000)
-                descString = descString.Substring(0, 2000);
-
-            builder.WithTitle($"Server Tags for {Context.Guild.Name}")
-                .WithColor(98, 31, 193)
-                .WithDescription(descString);
-            await ReplyAsync("", false, builder.Build());
+            await ReplyAsync("", embed: builder.Build());
         }
     }
 }
