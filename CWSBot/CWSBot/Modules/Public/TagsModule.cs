37b0b2167222dff76fffb5d2611c9b1eb581223c
Izumemori <rannick10@tutanota.com>
2017-10-27 13:51:44 +0200

    Minor improvements on TagModule

--- CWSBot/CWSBot/Modules/Public/TagsModule.cs
+++ CWSBot/CWSBot/Modules/Public/TagsModule.cs
@@ -15,31 +15,67 @@ namespace CWSBot.Modules.Public
     {
         private CwsContext _dbContext;
 
+        private Color _embedColor;
+
+        protected override void BeforeExecute(CommandInfo command)
+        {
+            if (Context.Channel is IGuildChannel)
+            {
+                _embedColor = (Context.User as SocketGuildUser).Roles.OrderByDescending(x => x.Position)
+                                                                     .Where(x => x.Color.RawValue != Color.Default.RawValue)
+                                                                     .FirstOrDefault()?.Color ?? Color.Purple;
+            }
+            else _embedColor = Color.Purple;
+        }
+
         public TagsModule(CwsContext dbContext)
         {
             _dbContext = dbContext;
         }
 
         [Command]
-        public async Task TagAsync(string Name)
+        public async Task TagAsync(string name)
         {
             //initialise the builder so we can use it later
-            EmbedBuilder builder = new EmbedBuilder();
+            var builder = new EmbedBuilder();
+
+            builder.Color = _embedColor;
             //retrieve the tag by name
-            Tag recievedTag = _dbContext.Tags.SingleOrDefault(t => t.Name.ToLower() == Name.ToLower() && t.GuildId == Context.Guild.Id);
+            var recievedTag = _dbContext.Tags.SingleOrDefault(t => t.Name.ToLower() == name.ToLower() && t.GuildId == Context.Guild.Id);
             //handle null case outside
             if (recievedTag != null)
             {
                 //check for URLs to make sure we don't wrap them in the embed
-                string[] strArray = recievedTag.Content.Split(" ");
+                var strArray = recievedTag.Content.Split(" ");
 
-                bool containsUrl = strArray.Any(s => Uri.TryCreate(s, UriKind.Absolute, out Uri UriResult)
+                var containsUrl = strArray.Any(s => Uri.TryCreate(s, UriKind.Absolute, out var UriResult)
                         && (UriResult.Scheme == Uri.UriSchemeHttp || UriResult.Scheme == Uri.UriSchemeHttps)
                         && UriResult != null);
                 //if it is a URL let's send it off without the embed!
                 if (containsUrl)
                 {
-                    await ReplyAsync(string.Format("{0}: \n{1}", recievedTag.Name, recievedTag.Content));
+                    // check for images
+                    var image = strArray.FirstOrDefault(x => Uri.TryCreate(x, UriKind.Absolute, out var res) && res.PointsToImage());
+
+                    // if there's an image then put it as embed image
+                    if (image != null)
+                    {
+                        var description = string.Join(" ", strArray.Where(x => x != image));
+
+                        builder.WithTitle(recievedTag.Name)
+                               .WithFooter("Owner: " + recievedTag.CreatorName);
+
+                        if (!string.IsNullOrWhiteSpace(description))
+                            builder.Description = description;
+
+                        builder.ImageUrl = image;
+
+                        await ReplyAsync("", embed: builder.Build());
+                    }
+                    else
+                    {
+                        await ReplyAsync(string.Format("{0}: \n{1}", recievedTag.Name, recievedTag.Content));
+                    }
 
                     recievedTag.Uses += 1;
                     _dbContext.SaveChanges();
@@ -48,48 +84,40 @@ namespace CWSBot.Modules.Public
                 }
                 //build our embed and send it off!
                 builder.WithTitle(recievedTag.Name)
-                    .WithColor(98, 31, 193)
                     .WithDescription(recievedTag.Content)
                     .WithFooter("Owner: " + recievedTag.CreatorName);
 
-                await ReplyAsync("", false, builder.Build());
+                await ReplyAsync("", embed: builder.Build());
                 recievedTag.Uses += 1;
                 return;
             }
-            //lists to store tag names
-            List<string> tagNames = new List<string>();
-            List<string> tagNames_ = new List<string>();
+
             //add tag names to first list
             var tags = _dbContext.Tags.Where(x => x.GuildId == Context.Guild.Id);
-            foreach (Tag tag in tags)
-            {
-                double Distance = Levenshtein.Compute(Name, tag.Name);
 
-                if (Distance / Name.Length <= 0.42857142857)
-                    tagNames.Add(tag.Name);
-            }
+            var tagNames = tags.AsEnumerable()
+                               .Select(x => (Distance: Levenshtein.Compute(name, x.Name) / x.Name.Length, Tag: x))
+                               .Where(t => t.Distance <= 0.42857142857)
+                               .Select(x => x.Tag); 
 
             if (tagNames.Count() == 0)
             {
                 builder.WithTitle("No matching tags found")
                     .WithDescription("There were no tags found matching that description. Use <tag list> to find some!");
                 await ReplyAsync("", false, builder.Build());
-            }
-            //add all the tags in the first list to the second one, just with a separator every n names.
-            for (int i = 0; i < 20 && i < tagNames.Count(); i++)
-            {
-                var separator = i % 4 == 0 && i != 0 ? "\n" : ", ";
 
-                tagNames_.Add(tagNames[i] + separator);
+                return;
             }
-            string response = string.Join(", ", tagNames_);
+
+            string response = string.Join("", tagNames.Select((x, i) => $"{x.Name}{(i % 3 == 0 && i != 0 ? "\n" : ", ")}"));
             response = response.Remove(response.Length - 2);
 
             builder.WithTitle("Tag not found!")
                 .WithDescription("Similar tags: \n" + response);
             //send result to user
-            await ReplyAsync("", false, builder.Build());
+            await ReplyAsync("", embed: builder.Build());
         }
+
         [Command("mod"), RequireUserPermission(GuildPermission.ManageGuild)]
         public async Task ModTagAsync(string args, string Name, [Remainder] string content = null)
         {
