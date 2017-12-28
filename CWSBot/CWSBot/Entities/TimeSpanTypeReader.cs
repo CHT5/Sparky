3ab02456d2a4af16f8877cf4dbef64b790a3569e
Izumemori <rannick10@tutanota.com>
2017-12-28 14:47:06 +0100

    Added `d/day/days`, `w/week/weeks`and `y/year/years` as valid timespan components

--- CWSBot/CWSBot/Entities/TimeSpanTypeReader.cs
+++ CWSBot/CWSBot/Entities/TimeSpanTypeReader.cs
@@ -7,30 +7,39 @@ namespace CWSBot.Entities
 {
     public class TimeSpanTypeReader : TypeReader
     {
-        private readonly string _pattern = "((?<hours>[0-9]+)(hours|hour|h))?((?<minutes>[0-9]+)(minutes|minute|min|m))?((?<seconds>[0-9]+)(seconds|second|s))?"; 
+        private const string Pattern = "((?<years>[0-9]+)(years|year|y))?((?<weeks>[0-9]+)(weeks|week|w))?((?<days>[0-9]+)(days|day|d))?((?<hours>[0-9]+)(hours|hour|h))?((?<minutes>[0-9]+)(minutes|minute|min|m))?((?<seconds>[0-9]+)(seconds|second|s))?"; 
 
         public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider provider)
         {
-            var _match = Regex.Match(input, _pattern);
+            var match = Regex.Match(input, Pattern);
 
-            if (_match is null) 
+            if (match is null) 
                 return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, "Failed to parse TimeSpan"));
-        
-            if (!int.TryParse(_match.Groups["hours"].Value, out int _hours) && !string.IsNullOrEmpty(_match.Groups["hours"].Value))
+
+            if (!int.TryParse(match.Groups["years"].Value, out int years) && !string.IsNullOrEmpty(match.Groups["years"].Value))
+                return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, "Too large number given for years"));
+
+            if (!int.TryParse(match.Groups["weeks"].Value, out int weeks) && !string.IsNullOrEmpty(match.Groups["weeks"].Value))
+                return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, "Too large number given for weeks"));
+
+            if (!int.TryParse(match.Groups["days"].Value, out int days) && !string.IsNullOrEmpty(match.Groups["days"].Value))
+                return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, "Too large number given for days"));
+
+            if (!int.TryParse(match.Groups["hours"].Value, out int hours) && !string.IsNullOrEmpty(match.Groups["hours"].Value))
                 return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, "Too large number given for hours"));
         
-            if (!int.TryParse(_match.Groups["minutes"].Value, out int _minutes) && !string.IsNullOrEmpty(_match.Groups["minutes"].Value))
+            if (!int.TryParse(match.Groups["minutes"].Value, out int minutes) && !string.IsNullOrEmpty(match.Groups["minutes"].Value))
                 return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, "Too large number given for minutes"));
 
-            if (!int.TryParse(_match.Groups["seconds"].Value, out int _seconds) && !string.IsNullOrEmpty(_match.Groups["seconds"].Value))
+            if (!int.TryParse(match.Groups["seconds"].Value, out int seconds) && !string.IsNullOrEmpty(match.Groups["seconds"].Value))
                 return Task.FromResult(TypeReaderResult.FromError(CommandError.ParseFailed, "Too large number given for seconds"));
             
-            var _timespan = new TimeSpan(_hours, _minutes, _seconds);
+            var timespan = new TimeSpan(days + weeks*7 + years*365, hours, minutes, seconds);
 
-            if (_timespan.TotalSeconds == 0)
+            if (timespan.TotalSeconds == 0)
                 return Task.FromResult(TypeReaderResult.FromError(CommandError.UnmetPrecondition, "The timespan has to be at least 1 second long!"));
 
-            return Task.FromResult(TypeReaderResult.FromSuccess(_timespan));
+            return Task.FromResult(TypeReaderResult.FromSuccess(timespan));
         }
     }
 }
